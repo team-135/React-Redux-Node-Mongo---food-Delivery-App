3e03e1970e7179b76d091e641413d3d328b155ad
nowshad-netizen <lewsedmond@gmail.com>
2022-01-03 00:38:27 +0600

    ProductDetails page added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Login from './Pages/Login/Login';
 import Dashboard from './Pages/Dashboard/Dashboard';
+import PrivateRoute from './PrivateRoute/PrivateRoute';
+import ProductDetails from './Pages/ProductDetails/ProductDetails';
 
 function App() {
   return (
@@ -29,9 +31,12 @@ function App() {
             <Route exact path="/login">
               <Login></Login>
             </Route>
-            <Route exact path="/dashboard">
+            <PrivateRoute exact path="/dashboard">
               <Dashboard></Dashboard>
-            </Route>
+            </PrivateRoute>
+            <PrivateRoute exact path="/ProductDetails">
+              <ProductDetails></ProductDetails>
+            </PrivateRoute>
             <Route exact path="*">
               <PageNotFound></PageNotFound>
             </Route>
